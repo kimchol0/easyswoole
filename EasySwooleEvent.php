761ee6dda7f47e21728665a123a3e83e133d3c8d
KimChol <jz_mewtwo@163.com>
2021-02-28 15:24:42 +0800

    --

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -16,25 +16,15 @@ class EasySwooleEvent implements Event
     public static function initialize()
     {
         date_default_timezone_set('Asia/Shanghai');
-        Di::getInstance()->set(SysConst::HTTP_EXCEPTION_HANDLER,[ExceptionHandler::class,'handle']);
+        //Di::getInstance()->set(SysConst::HTTP_EXCEPTION_HANDLER,[ExceptionHandler::class,'handle']);
     }
 
     public static function mainServerCreate(EventRegister $register)
     {
-        $processConfig = new Config();
-        $processConfig->setProcessName('test1');
-        $processConfig->setArg([
-            'size'>=3
-        ]);
-        $obj = new ProcessOne($processConfig);
-        try{
-            ServerManager::getInstance()->addProcess($obj);
-        }catch (\Exception $e){
-        }
+
     }
     public static function onRequest(Request $request,Response $response):bool
     {
-        $process = ServerManager::getInstance()->getProcess('test1');
-        $process->write('clear');
+
     }
 }
